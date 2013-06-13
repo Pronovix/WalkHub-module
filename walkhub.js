c5882c44d52fb3e0b9bb16e8bed6097d82d87e27
Tamás Demeter-Haludka <yorirou@gmail.com>
2013-06-13 15:04:09 +0200

    Adds multiple server and proxy support.

--- walkhub.js
+++ walkhub.js
@@ -5,94 +5,116 @@
     return window.location.protocol + '//' + window.location.hostname + Drupal.settings.basePath;
   }
 
-  var state = {
-    walkthrough: null,
-    step: null,
-    completed: false,
-    stepIndex: 0
-  };
+  function WalkhubServer() {
+    var key = Math.random().toString();
 
-  var handlers = {
-    connect: function (data, source) {
-      walkthroughOrigin = data.origin;
-      post({
-        type: 'connect_ok',
-        origin: window.location.origin,
-        baseurl: baseurl()
-      }, source);
-    },
-    request: function (data, source) {
-      var opts = {
-        url: data.URL,
-        method: 'GET',
-        success: function (respdata) {
-          post({
-            ticket: data.ticket,
-            type: 'success',
-            data: respdata
-          }, source);
-        },
-        error: function (xhr, status, err) {
-          post({
-            ticket: data.ticket,
-            type: 'error',
-            error: err,
-            status: status
-          }, source);
-        },
-        dataType: 'json',
-        accept: 'application/json'
-      };
+    var state = {
+      walkthrough: null,
+      step: null,
+      completed: false,
+      stepIndex: 0
+    };
 
-      if (data.data) {
-        opts.data = data;
-        opts.method = 'POST';
+    function maybeProxy(newdata, olddata) {
+      if (olddata.proxy_key) {
+        newdata.proxy_key = olddata.proxy_key;
       }
-
-      $.ajax(opts);
-    },
-    getState: function (data, source) {
-      post({
-        type: 'state',
-        state: state
-      }, source);
-    },
-    setState: function (data, source) {
-      console.log("State updated", data.state);
-      state = data.state;
-    },
-    log: function (data, source) {
-      // TODO set a variable to enable/disable logging
-      window.console && console.log && console.log('REMOTE LOG', data.log);
+      return newdata;
     }
-  };
 
-  function post(message, source) {
-    if (source.postMessage) {
-      source.postMessage(JSON.stringify(message), walkthroughOrigin);
-    } else {
-      window.console && console.log && console.log('Sending message failed.');
+    var handlers = {
+      connect: function (data, source) {
+        walkthroughOrigin = data.origin;
+        post(maybeProxy({
+          type: 'connect_ok',
+          origin: window.location.origin,
+          baseurl: baseurl(),
+          key: key
+        }, data), source);
+      },
+      request: function (data, source) {
+        var opts = {
+          url: data.URL,
+          method: 'GET',
+          success: function (respdata) {
+            post(maybeProxy({
+              ticket: data.ticket,
+              type: 'success',
+              data: respdata
+            }, data), source);
+          },
+          error: function (xhr, status, err) {
+            post(maybeProxy({
+              ticket: data.ticket,
+              type: 'error',
+              error: err,
+              status: status
+            }, data), source);
+          },
+          dataType: 'json',
+          accept: 'application/json'
+        };
+
+        if (data.data) {
+          opts.data = data;
+          opts.method = 'POST';
+        }
+
+        $.ajax(opts);
+      },
+      getState: function (data, source) {
+        post(maybeProxy({
+          type: 'state',
+          state: state
+        }, data), source);
+      },
+      setState: function (data, source) {
+        console.log("State updated", data.state);
+        state = data.state;
+      },
+      log: function (data, source) {
+        // TODO set a variable to enable/disable logging
+        window.console && console.log && console.log('REMOTE LOG', data.log);
+      }
+    };
+
+    handlers.connect.keyBypass = true;
+
+    function post(message, source) {
+      if (source.postMessage) {
+        source.postMessage(JSON.stringify(message), walkthroughOrigin);
+      } else {
+        window.console && console.log && console.log('Sending message failed.');
+      }
     }
+
+    window.addEventListener('message', function (event) {
+      var data = JSON.parse(event.data);
+      var handler = data && data.type && handlers[data.type];
+      if (handler && (handler.keyBypass || (data.key && data.key == key))) {
+        console.log(event);
+        handler(data, event.source);
+      } else {
+        console.log('Message discarded', event);
+      }
+    });
+
+    this.clickEventHandler = function (event) {
+      event.preventDefault();
+      state.walkthrough = $(this).attr('data-walkthrough-uuid');
+      state.step = null;
+      window.open(baseurl() + 'walkhub#' + window.location.origin);
+    };
   }
-
-  window.addEventListener('message', function (event) {
-    var data = JSON.parse(event.data);
-    if (data && data.type && handlers[data.type]) {
-      console.log(event);
-      handlers[data.type](data, event.source);
-    }
-  });
 
   Drupal.behaviors.walkhub = {
     attach: function (context) {
       $('.walkthrough-start:not(.walkhub-processed)', context)
         .addClass('walkhub-processed')
-        .click(function (event) {
-           event.preventDefault();
-           state.walkthrough = $(this).attr('data-walkthrough-uuid');
-           state.step = null;
-           window.open(baseurl() + 'walkhub#' + window.location.origin);
-         });
+        .each(function () {
+          var appserver = new WalkhubServer();
+          $(this).click(appserver.clickEventHandler);
+        });
     }
   };
 })(jQuery);
