028d1b4a0299835926b855b7fbb2f754fd703d55
Kornel Lugosi <coornail@gmail.com>
2014-02-11 09:19:42 +0000

    Fix missing space before :

--- walkhub.js
+++ walkhub.js
@@ -1,6 +1,6 @@
 // Add window.location.origin for browsers which doesn't support it.
 if (!window.location.origin) {
-  window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+  window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
 }
 
 // Add console log function if not availible.
