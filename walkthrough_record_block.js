fe115f1fd8dda48ffb076c41c27e5028f6f07074
Árpád Csányi <csarpad@gmail.com>
2014-06-20 12:18:00 +0200

    feature/505+483: create recorder popup
    Fixing errors reported by reviewers.

--- walkthrough_record_block.js
+++ walkthrough_record_block.js
@@ -1,21 +1,23 @@
 (function($) {
+  "use strict";
   $(document).ready(function() {
 
-    var url_re = /^http\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]+(\/\S*)?$/;
+    var url_re = /^https?:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]+(\/\S*)?$/;
+    var DEFAULT_URL = "http://pronovix.com/contact-us";
 
     $("#record-a-walkthrough-button").click(function (e) {
-      var url_field = $("#walkthrough-record-url");
+      var $url_field = $("#walkthrough-record-url");
       $(this).hide();
       $("#record-a-walkthrough-popup-modal").show();
-      if (url_field.val() == "") {
-        url_field.val("http://pronovix.com/contact-us");
+      if ($url_field.val() === "") {
+        $url_field.val(DEFAULT_URL);
         $("#url-error").hide();
       }
-      url_field.select();
+      $url_field.select();
     });
 
     $("#walkthrough-record-url").keyup(function(e) {
-      if (e.keyCode == 13) {
+      if (e.keyCode === 13) {
         $("#walkthrough-start-recording").click();
       }
     });
@@ -29,19 +31,19 @@
         $("#edit-url").val(url);
         $("#edit-record").click();
 
-        var title = $("#walkthrough-record-title-wrapper");
-        var severity = $("#walkthrough-record-severity-wrapper");
-        title.find("label").text($(".form-item-title label").text());
+        var $title = $("#walkthrough-record-title-wrapper");
+        var $severity = $("#walkthrough-record-severity-wrapper");
+        $title.find("label").text($(".form-item-title label").text());
         var title_input = $("#edit-title").clone();
         title_input.attr("id","walkthrough-record-title");
-        title.find(".form-element").html(title_input);
-        title.find("label").attr("for", "walkthrough-record-title")
+        $title.find(".form-element").html(title_input);
+        $title.find("label").attr("for", "walkthrough-record-title");
 
-        severity.find("label").text($(".form-item-severity label").text());
+        $severity.find("label").text($(".form-item-severity label").text());
         var severity_select = $("#edit-severity").clone();
         severity_select.attr("id","walkthrough-record-severity");
-        severity.find(".form-element").html(severity_select);
-        severity.find("label").attr("for", "walkthrough-record-severity");
+        $severity.find(".form-element").html(severity_select);
+        $severity.find("label").attr("for", "walkthrough-record-severity");
 
         $("#walkthrough-record-first-step").hide();
         $("#walkthrough-record-second-step").show();
@@ -54,32 +56,32 @@
     });
 
     $("#steps").bind('DOMSubtreeModified', function(e) {
-      var stepsBox = $("#walkthrough-recorded-steps");
-      var steps = $("#steps");
+      var $stepsBox = $("#walkthrough-recorded-steps");
+      var $steps = $("#steps");
       if (e.target.innerHTML.length > 0) {
-        stepsBox.val(steps.text().replace(/\)/g,")\n"));
+        $stepsBox.val($steps.text().replace(/\)/g,")\n"));
       }
     });
 
     $("#walkthrough-record-save").click(function() {
-      var input_title = $("#walkthrough-record-title");
-      var select_severity = $("#walkthrough-record-severity");
+      var $input_title = $("#walkthrough-record-title");
+      var $select_severity = $("#walkthrough-record-severity");
       var error = false;
-      if (input_title.val() == "") {
+      if ($input_title.val() === "") {
         error = true;
         $("#walkthrough-record-title-error").show();
       } else {
         $("#walkthrough-record-title-error").hide();
       }
-      if (select_severity.val() == "") {
+      if ($select_severity.val() === "") {
         error = true;
         $("#walkthrough-record-severity-error").show();
       } else {
         $("#walkthrough-record-severity-error").hide();
       }
       if (!error) {
-        $("#edit-title").val(input_title.val());
-        $("#edit-severity").val(select_severity.val());
+        $("#edit-title").val($input_title.val());
+        $("#edit-severity").val($select_severity.val());
         $("#edit-save").click();
       }
     });
