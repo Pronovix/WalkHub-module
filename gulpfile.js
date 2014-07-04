47902daf66c6f5d43e550318517cef4f5b504264
Kornel Lugosi <coornail@gmail.com>
2014-07-04 20:04:27 +0100

    Add gulpfile for checking eslint

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+(function () {
+  "use strict";
+
+  var gulp = require("gulp");
+
+  var args = require("yargs").argv;
+  var uglify = require("gulp-uglify");
+  var eslint = require("gulp-eslint");
+
+  var paths = {
+    non_vendor_scripts: [
+      "./embed.js",
+      // walkhub-ie-fixes.js adds hacks for IE, it does not conform eslint for now:
+      // "./walkhub-ie-fixes.js",
+      "./walkhub.js",
+      "./walkthrough_record_block.js",
+    ],
+  };
+
+  gulp.task("eslint", function () {
+    return gulp.src(paths.non_vendor_scripts)
+      .pipe(eslint({
+        env: {
+          "browser": true
+        },
+        rules: {
+          "eqeqeq": [2, "smart"],
+          "guard-for-in": 2,
+          "no-undef": 2,
+          "no-unused-vars": 0,
+          "strict": 2,
+          "new-cap": 0,
+          "quotes": 0,
+          "camelcase": 0,
+          "no-underscore-dangle": 0,
+          "no-new": 0,
+          "no-alert": 0,
+          "no-use-before-define": 0,
+          "consistent-return": 0,
+          "no-constant-condition": 0,
+          "no-console": 0
+        },
+        globals: {
+          'console': true,
+          'jqWalkhub': true,
+          'Drupal': true,
+          'window': true,
+          'jQuery': true
+        },
+      }))
+      .pipe(eslint.format())
+      .pipe(eslint.failOnError());
+  });
+
+  gulp.task("watch", function () {
+    gulp.watch(paths.non_vendor_scripts, ["eslint"]);
+  });
+
+  gulp.task("default", ["eslint"]);
+})();
+
